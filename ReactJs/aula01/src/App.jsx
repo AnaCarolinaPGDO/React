150f272ca67e9725060a8117a8c281f5a7dedc53
Ana Carolina <50124714+AnaCarolinaPGDO@users.noreply.github.com>
2024-10-31 18:32:39 -0300

    f

--- ReactJs/aula01/src/App.jsx
+++ ReactJs/aula01/src/App.jsx
@@ -1,15 +1,14 @@
-import { AulaUseMemo } from "./pages/Aula-useMemo"
-import { Login } from './pages/Login'
+// import { AulaUseMemo } from "./pages/Aula-useMemo"
+// import { Login } from './pages/Login'
 
-export const App = () => {
-  return (
-    <Login />
-  )
-}
+// export const App = () => {
+//   return (
+//     <Login />
+//   )
+// }
 
 // useEffect
 // Imutabilidade
 // Componente de Decisao
 // Rotas
 // Api axios
-{ Login } 
