24956804d61c1867c04b25291beff2f7d2ab08aa
Ana Carolina <50124714+AnaCarolinaPGDO@users.noreply.github.com>
2024-10-30 23:52:23 -0300

    im so sad bruh

---
+++ ReactJs/primeira-aula01/src/App.jsx
@@ -0,0 +1,13 @@
+import { AulaUseMemo } from "./pages/Aula-useMemo"
+
+export const App = () => {
+  return (
+    <AulaUseMemo />
+  )
+}
+
+// useEffect
+// Imutabilidade
+// Componente de Decisao
+// Rotas
+// Api axios
