63dcd539fa9a77dc0fac93710a4102e25d1613ac
Ana Carolina <50124714+AnaCarolinaPGDO@users.noreply.github.com>
2024-10-31 00:08:36 -0300

    çlçpl

--- bruh/primeira-aula01/src/components/InputField/style.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const InputStyle = styled.input`
-  background-color: #fff;
-  border: none;
-  border-radius: 2px;
-  width: 100%;
-  height: 40px;
-`;
