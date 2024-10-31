31b2db247ae9220180daf1aa3334d7d708b38874
Ana Carolina <50124714+AnaCarolinaPGDO@users.noreply.github.com>
2024-10-31 20:25:02 -0300

    AEAEEEEEEEEE

--- ReactJs/aula01/src/pages/RequestApi/index.jsx
+++ ReactJs/aula01/src/pages/RequestApi/index.jsx
@@ -1,10 +1,37 @@
+import { useEffect } from 'react';
+import { Container } from './style';
+import axios from 'axios'
+
+export const RequestApi = () => {
+
+    // const carregar = async () => {};
+
+    async function carregarDadosApi(){
+
+        const url = 'https://66f4ad8277b5e889709a277e.mockapi.io/api/v1/users'
+
+        try {
+            const resposta = await axios.get(url);
 
 
-export const RequestApi() => {
+            if (resposta.status === 200) {
+                console.log('informações da api', resposta.data)
+            } else {
+                console.log('Errouu')
+            }
+        }
+        catch (error) {
+            console.log(`Error: ${error}`)
+        }
+    }
+
+    useEffect(() => {
+        carregarDadosApi();
+    }, [])
 
     return (
-        <div>
-            requestAPI
-        </div>
+        <Container>
+            RequestAPI
+        </Container>
     )
 }
---
+++ ReactJs/aula01/src/pages/RequestApi/style.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: #a3a3b9;
+  height: 100vh;
+  `;
